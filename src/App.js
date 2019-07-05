cc980878b3d8a94c364762546963bf165fc2aca5
Peanut <michjmend@gmail.com>
2019-07-04 20:07:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import images from "./images.json";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Clicky Game</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Click an image to begin!</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Score:</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Top Score:</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Clicky Game!</h1>
+        <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
+      </div>
     </div>
+    </>
   );
 }
 
